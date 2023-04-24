844d3d121c76a4207dddc39e6bf69a695ea8ca19
CrimsonCoder42 <nostro37@gmail.com>
2023-04-24 18:06:10 -0400

    test 13 ci/cd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Layout from '../components/Layout';
+import { Link } from 'gatsby';
 import '../styles/page.css';
 
 const IndexPage = ({ data }) => {
@@ -19,28 +20,24 @@ const IndexPage = ({ data }) => {
           <div className="container">
             <ul className="image-grid">
               <li>
-                <a href="/public/movies/tt2911666">
-                  <img src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"
-                       alt="Image 1" />
-                </a>
+                <Link to="/movies/tt2911666">
+                  <img src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg" alt="Image 1" />
+                </Link>
               </li>
               <li>
-                <a href="/public/movies/tt4425200">
-                  <img src="https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg"
-                       alt="Image 2" />
-                </a>
+                <Link to="/movies/tt4425200">
+                  <img src="https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg" alt="Image 2" />
+                </Link>
               </li>
               <li>
-                <a href="/public/movies/tt6146586">
-                  <img src="https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"
-                       alt="Image 3" />
-                </a>
+                <Link to="/movies/tt6146586">
+                  <img src="https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg" alt="Image 3" />
+                </Link>
               </li>
               <li>
-                <a href="/public/movies/tt10366206">
-                  <img src="https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg"
-                       alt="Image 4" />
-                </a>
+                <Link to="/movies/tt10366206">
+                  <img src="https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg" alt="Image 4" />
+                </Link>
               </li>
             </ul>
           </div>
