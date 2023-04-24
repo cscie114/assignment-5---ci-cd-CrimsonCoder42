0796129b99152d8b3509d0257fac1471a1099c66
CrimsonCoder42 <nostro37@gmail.com>
2023-04-24 17:56:49 -0400

    test 12 ci/cd

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['./node_modules/ts-standardx/.eslintrc.js'],
+    env: {
+        browser: true,
+        node: true,
+        jest: true
+    },
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.tsx', '*.ts'],
+            rules: {
+                'prettier/prettier': 0
+            }
+        }
+    ]
+}
