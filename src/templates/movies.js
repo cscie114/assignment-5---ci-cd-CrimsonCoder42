551ec60d97cf664a07b7238a9a4eebc0bf5a07d5
CrimsonCoder42 <nostro37@gmail.com>
2023-04-24 18:45:52 -0400

    test 14 ci/cd - dependecies updated

--- src/templates/movies.js
+++ src/templates/movies.js
@@ -11,13 +11,22 @@ const Movies = ({ pageContext }) => {
     return (
         <Layout>
             <div className="container">
-            <h1>
-                { movie.Title }
-            </h1>
-            <p>Year: { movie.Year }</p>
-            <p>Rated: { movie.Rated }</p>
-            <p>{ movie.Plot }</p>
-            <img src={movie.Poster} alt={movie.Title} />
+                <h1>{movie.Title}</h1>
+                <img src={movie.Poster} alt={movie.Title} />
+                <p>Year: {movie.Year}</p>
+                <p>Rated: {movie.Rated}</p>
+                <p>Released: {movie.Released}</p>
+                <p>Runtime: {movie.Runtime}</p>
+                <p>Genre: {movie.Genre}</p>
+                <p>Director: {movie.Director}</p>
+                <p>Writer: {movie.Writer}</p>
+                <p>Actors: {movie.Actors}</p>
+                <p>Plot: {movie.Plot}</p>
+                <p>Language: {movie.Language}</p>
+                <p>Country: {movie.Country}</p>
+                <p>Awards: {movie.Awards}</p>
+                <p>IMDb Rating: {movie.imdbRating}</p>
+                <p>IMDb Votes: {movie.imdbVotes}</p>
             </div>
         </Layout>
     )
