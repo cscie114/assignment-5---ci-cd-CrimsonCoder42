1b252a04fd1daaaeca82839adf12c0c54c0c4241
CrimsonCoder42 <nostro37@gmail.com>
2023-04-24 17:43:02 -0400

    test 11 ci/cd

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,25 +19,25 @@ const IndexPage = ({ data }) => {
           <div className="container">
             <ul className="image-grid">
               <li>
-                <a href="/movies/tt2911666/">
+                <a href="/movies/tt2911666">
                   <img src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"
                        alt="Image 1" />
                 </a>
               </li>
               <li>
-                <a href="/movies/tt4425200/">
+                <a href="/movies/tt4425200">
                   <img src="https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_SX300.jpg"
                        alt="Image 2" />
                 </a>
               </li>
               <li>
-                <a href="/movies/tt6146586/">
+                <a href="/movies/tt6146586">
                   <img src="https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"
                        alt="Image 3" />
                 </a>
               </li>
               <li>
-                <a href="/movies/tt10366206/">
+                <a href="/movies/tt10366206">
                   <img src="https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg"
                        alt="Image 4" />
                 </a>
